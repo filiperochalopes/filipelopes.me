a79e41cb906a1a5ed34a7c1ce42f6f078b099f18
Ckk3 <contato.gustavomota@gmail.com>
2023-02-07 22:06:03 -0300

    change font size and char limit

--- api/django_rest_api/django_rest_api/pdf/facade.py
+++ api/django_rest_api/django_rest_api/pdf/facade.py
@@ -15,11 +15,11 @@ def generate_pdf():
         pdf.set_font('Lato-Bold', 35)
         pdf.add_oneline_text(text=filipe_data['name'], pos=(294, 780), camp_name='Nome', len_max=100, centralized=True, interval=' ')
         
-        pdf.set_font('Lora-Regular', 12)
+        pdf.set_font('Lora-Regular', 10)
         pdf.add_oneline_text(text=f"{filipe_data.get('phone_number')}  |  {filipe_data.get('email')}  |  {filipe_data.get('website')}", pos=(294, 730), camp_name='Informacoes de Contato', len_max=100, centralized=True)
         pdf.add_rectangle(pos=(30, 697), width=535, height=1, fill=1, color=(0,0,0,0))
         pdf.add_rectangle(pos=(30, 690), width=535, height=1, fill=1, color=(0,0,0,0))
-        last_y_pos = pdf.add_morelines_text(text=filipe_data.get('about'), initial_pos=(30, 650), camp_name='Sobre', len_max=5000, decrease_ypos=16, char_per_lines=95)
+        last_y_pos = pdf.add_morelines_text(text=filipe_data.get('about'), initial_pos=(30, 650), camp_name='Sobre', len_max=5000, decrease_ypos=16, char_per_lines=110)
         new_y_pos = last_y_pos - (16*2)
         pdf.add_rectangle(pos=(30, new_y_pos), width=535, height=1, fill=1, color=(0,0,0,0))
         new_y_pos -= 40
