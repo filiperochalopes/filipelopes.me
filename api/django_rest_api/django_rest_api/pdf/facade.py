a892d8f6d39202161fddf0a9c71575b4910126d0
Ckk3 <contato.gustavomota@gmail.com>
2023-02-23 22:04:54 -0300

    changing filipe_data json with language

--- api/django_rest_api/django_rest_api/pdf/facade.py
+++ api/django_rest_api/django_rest_api/pdf/facade.py
@@ -10,12 +10,7 @@ import json
 def generate_pdf(relevance_level=3, lang:str='pt'):
     pdf = ReportLabCanvasUtils()
     try:
-        with open(settings.FILIPE_DATA_JSON, 'r') as f:
-            filipe_data = json.loads(f.read())
-        pdf.set_font('Lato-Bold', 35)
-        pdf.add_oneline_text(text=str(filipe_data['name']).upper(), pos=(294, 780), field_name='Nome', len_max=100, centralized=True, interval=' ')
-        
-        lang = 'en'
+        #lang = 'en'
         if lang == 'pt':
             # Variable to be used in titles
             language_is_portuguese = True
@@ -25,21 +20,24 @@ def generate_pdf(relevance_level=3, lang:str='pt'):
             pdf.default_language = 'en'
         else:
             raise Exception(f'selected language "{lang}" does not exist, please select "pt" or "en"')
+        
+        with open(settings.FILIPE_DATA_JSON, 'r') as f:
+            filipe_data = json.loads(f.read())
+        pdf.set_font('Lato-Bold', 35)
+
+        pdf.add_oneline_text(text=str(filipe_data[pdf.default_language].get('name')).upper(), pos=(294, 780), field_name='Nome', len_max=100, centralized=True, interval=' ')
+        
 
         pdf.set_font('Lora-Regular', 12)
-        profession_title_pt = 'Desenvolvedor Web e Médico'
-        profession_title_en = 'Web Developer and M.D./Physician'
-        if language_is_portuguese:
-            pdf.add_oneline_text(text=str(profession_title_pt).upper(), pos=(294, 747), field_name='Titulo profissional', len_max=100, centralized=True, interval=' ')
-        else:
-            pdf.add_oneline_text(text=str(profession_title_en).upper(), pos=(294, 747), field_name='Professional Title', len_max=100, centralized=True, interval=' ')
+        pdf.add_oneline_text(text=str(filipe_data[pdf.default_language].get('professional_title')).upper(), pos=(294, 747), field_name='Titulo profissional | Professional Title', len_max=100, centralized=True, interval=' ')
+        
 
         pdf.set_font('Lora-Regular', 10)
-        pdf.add_oneline_text(text=f"{filipe_data.get('phone_number')}  |  {filipe_data.get('email')}  |  {filipe_data.get('website')}", pos=(294, 700), field_name='Informacoes de Contato | Contact info', len_max=100, centralized=True)
+        pdf.add_oneline_text(text=f"{filipe_data[pdf.default_language].get('phone_number')}  |  {filipe_data[pdf.default_language].get('email')}  |  {filipe_data[pdf.default_language].get('website')}", pos=(294, 700), field_name='Informacoes de Contato | Contact info', len_max=100, centralized=True)
         # bars between contact info and about
         pdf.add_rectangle(pos=(30, 680), width=535, height=1, fill=1, color=(0,0,0,0))
         pdf.add_rectangle(pos=(30, 673), width=535, height=1, fill=1, color=(0,0,0,0))
-        last_y_pos = pdf.add_morelines_text(text=filipe_data.get('about'), initial_pos=(30, 630), field_name='Sobre | About', len_max=5000, decrease_ypos=16, char_per_lines=110)
+        last_y_pos = pdf.add_morelines_text(text=filipe_data[pdf.default_language].get('about'), initial_pos=(30, 630), field_name='Sobre | About', len_max=5000, decrease_ypos=16, char_per_lines=110)
         new_y_pos = last_y_pos - (16*2)
         # Bar between about and education
         pdf.add_rectangle(pos=(30, new_y_pos), width=535, height=1, fill=1, color=(0,0,0,0))
